b22a68c0008a5d16db624c065a6a9dd4d1de2366
J_DDev <69683068+JayyDoesDev@users.noreply.github.com>
2023-07-07 15:12:12 -0400

    Update statement.rb

--- statement.rb
+++ statement.rb
@@ -1,7 +1,29 @@
+name = "cole"
 hungry = true
 
 if hungry
     puts "go eat bro"
 else
     puts "bro is NOT hungry"
+end
+
+if hungry and name == "cole"
+    puts "cole if you're hungry go eat"
+else 
+    puts "ok"
+end
+
+
+if hungry 
+    puts "go eat bro"
+elsif name == "jay"
+    puts "ok"
+else
+ puts "test"
+end
+
+if !hungry and name == "jay"
+    puts "then go code loser"
+else 
+    puts "ok"
 end
