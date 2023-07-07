bf60b909586e77896d0a4149d6535b55ab712183
J_DDev <69683068+JayyDoesDev@users.noreply.github.com>
2023-07-07 15:26:53 -0400

    compare and case

---
+++ case.rb
@@ -0,0 +1,10 @@
+ok = true
+
+case ok
+when true
+    puts "ok"
+when false 
+    puts "not ok"
+else 
+    puts "i have no idea tbh"
+end
---
+++ compare.rb
@@ -0,0 +1,7 @@
+if 100344 > 2
+    puts true
+else
+    puts false
+end
+
+puts 100344 > 2
