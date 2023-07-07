55cb937a2f061f51deccdcd05dd25ebd3cc56f6c
J_DDev <69683068+JayyDoesDev@users.noreply.github.com>
2023-07-07 14:47:35 -0400

    Hash

---
+++ function.rb
@@ -0,0 +1,5 @@
+def default(string="bro?")
+    return puts string
+end
+
+default "some string or something idk"
---
+++ hash.rb
@@ -0,0 +1,10 @@
+consoles = {
+    "Xbox360" => "Microsoft",
+    "Xbox1" => "Microsoft",
+    "PS4" => "Sony",
+    "PS5" => "Sony",
+    :meatball => "meatballs are good ngl"
+}
+
+puts consoles["PS5"]
+puts consoles[:meatball]
