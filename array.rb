eb00ba601dafae772f8202905bb0aba96ff621b6
J_DDev <69683068+JayyDoesDev@users.noreply.github.com>
2023-07-07 14:25:42 -0400

    Converting and arrays

---
+++ array.rb
@@ -0,0 +1,7 @@
+array = Array["test", 0.2, "anothertestcausewhynot", "somethingelse", "cool", "foo", "bar"]
+
+# prints last element of the array
+puts array[array.length - 1]
+
+puts array.include? "test"
+
--- convert.rb
+++ convert.rb
@@ -1,6 +1,6 @@
 string = "223232343434"
 number = 1111121212222
-
+# converting to flow: to_f
 # converts to integer
 puts string.to_i
 # converts to string
